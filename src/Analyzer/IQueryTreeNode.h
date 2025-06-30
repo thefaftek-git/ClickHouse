28103fc41d72a75f01dd3932f111b17fb8837858
Nikolai Kochetov <nk@clickhouse.com>
2025-06-30 16:09:55 +0000

    Add updateHash to IDataType.

--- src/Analyzer/IQueryTreeNode.h
+++ src/Analyzer/IQueryTreeNode.h
@@ -291,8 +291,6 @@ protected:
     /// Subclass must convert its internal state and its children to AST
     virtual ASTPtr toASTImpl(const ConvertToASTOptions & options) const = 0;
 
-    static void updateHashForType(HashState & hash_state, const DataTypePtr & type);
-
     QueryTreeNodes children;
     QueryTreeWeakNodes weak_pointers;
 
